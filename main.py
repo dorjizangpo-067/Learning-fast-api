1ed2d00f9c9ba46e7789fbcba955d90fe58e26b4
dorjizangpo-067 <dorjizangpo067@gmail.com>
2025-12-23 14:48:40 +0600

    create model and update main

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from model import Products
 
 app = FastAPI()
 
 @app.get("/")
 def greet():
     return "Hello, World!"
+
+products = [
+    Products(id=1, name = "Phone", description = "Ram 12GB, Rom 126GB", price = 120.2, quantity=2),
+    Products(id = 2, name = "Laptop", description = "Ram 16GB, SSD 500GB", price = 600.2, quantity = 5)
+]
+
+@app.get("/products")
+def get_all_products():
+    return products
